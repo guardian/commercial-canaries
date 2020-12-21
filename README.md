7ab3672cae818e375bab36bb8eee607073ba9966
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-12-21 20:35:08 +0200

    Update code and README.md

--- README.md
+++ README.md
@@ -7,3 +7,20 @@
 
 1. [Node 14](https://nodejs.org/en/download/) ([nvm](https://github.com/nvm-sh/nvm))
 2. [Yarn](https://classic.yarnpkg.com/en/docs/install/)
+
+### Manual Update
+
+There are currently 3 canary tests running that check the CMP is loaded successfully and ads are 
+loaded at the right time in US, AUS and UK respectively.
+
+Code | AWS Region | Canary configuration name | 
+| :---:   | :---:   | :-: | :-: |
+| `src/cmp_tcfv2` | eu-west-1 | commercial_cmp_tcfv2 | 
+| `src/cmp_ccpa`  | us-west-1 | commercial_cmp_us | 
+| `src/cmp_aus` | ap-southeast-2 | commercial_cmp_aus | 
+
+Login to AWS frontend via Janus, and replace the code via the build in Script Editor
+
+### Automatic Update
+
+This is a work in progress
--- src/cmp_ccpa.js
+++ src/cmp_ccpa.js
@@ -2,6 +2,8 @@ const synthetics = require('Synthetics');
 const log = require('SyntheticsLogger');
 
 const pageLoadBlueprint = async function () {
+
+    // INSERT URL here
     const URL = "https://www.theguardian.com/us";
 
     let page = await synthetics.getPage();
@@ -17,15 +19,13 @@ const pageLoadBlueprint = async function () {
     }
 
     //Ads loaded before interacting with CMP
-    await page.waitForSelector('.ad-slot--top-above-nav .ad-slot__label');
     await page.waitForSelector('.ad-slot--top-above-nav .ad-slot__content iframe');
-    //await page.waitFor(1000);
 
     // wait for CMP
     await page.waitForSelector('[id*="sp_message_container"]');
     log.info('CMP loaded');
 
-    // click Do not sell my information
+    //click Do not sell my information
     const frame = page.frames().find(f => f.url().startsWith('https://ccpa-notice.sp-prod.net'));
     await frame.click('button[title="Do not sell my personal information"]');
 
