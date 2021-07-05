ac32c04328552e988b481f9608d89c933630c667
Chris Jones <chrislomaxjones96@gmail.com>
2021-07-05 11:51:40 +0100

    Format src files

--- src/cmp_aus.js
+++ src/cmp_aus.js
@@ -2,43 +2,48 @@ const synthetics = require('Synthetics');
 const log = require('SyntheticsLogger');
 
 const checkPage = async function (URL) {
-    log.info(`Checking Page URL ${URL}`);
+	log.info(`Checking Page URL ${URL}`);
 
-    let page = await synthetics.getPage();
+	let page = await synthetics.getPage();
 
-    //clear cookies
-    const client = await page.target().createCDPSession();
-    await client.send('Network.clearBrowserCookies');
+	//clear cookies
+	const client = await page.target().createCDPSession();
+	await client.send('Network.clearBrowserCookies');
 
-    const response = await page.goto(URL, {waitUntil: 'domcontentloaded', timeout: 30000});
-    if (!response) {
-        throw "Failed to load page!";
-    }
+	const response = await page.goto(URL, {
+		waitUntil: 'domcontentloaded',
+		timeout: 30000,
+	});
+	if (!response) {
+		throw 'Failed to load page!';
+	}
 
-    // wait for CMP
-    await page.waitForSelector('[id*="sp_message_container"]');
-    log.info('CMP loaded');
+	// wait for CMP
+	await page.waitForSelector('[id*="sp_message_container"]');
+	log.info('CMP loaded');
 
-    //await synthetics.takeScreenshot('loaded', 'loaded');
+	//await synthetics.takeScreenshot('loaded', 'loaded');
 
-    //Ads loaded before interacting with CMP
-    // Uncomment when AUS house ads are fixed
-    //await page.waitForSelector('.ad-slot--top-above-nav .ad-slot__content iframe');
+	//Ads loaded before interacting with CMP
+	// Uncomment when AUS house ads are fixed
+	//await page.waitForSelector('.ad-slot--top-above-nav .ad-slot__content iframe');
 
-    //click OK
-    const frame = page.frames().find(f => f.url().startsWith('https://ccpa-notice.sp-prod.net'));
-    await page.waitFor(2000);
-    await frame.click('button[title="Continue"]')
+	//click OK
+	const frame = page
+		.frames()
+		.find((f) => f.url().startsWith('https://ccpa-notice.sp-prod.net'));
+	await page.waitFor(2000);
+	await frame.click('button[title="Continue"]');
 };
 
 const pageLoadBlueprint = async function () {
-    // Check Front
-    await checkPage("https://www.theguardian.com");
+	// Check Front
+	await checkPage('https://www.theguardian.com');
 
-    // Check Article
-    // await checkPage("https://www.theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake");
+	// Check Article
+	// await checkPage("https://www.theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake");
 };
 
 exports.handler = async () => {
-    return await pageLoadBlueprint();
+	return await pageLoadBlueprint();
 };
