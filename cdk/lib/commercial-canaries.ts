2c320fd4c091e54fce08beebc09f88d3ec632fe7
Dominik Lander <lander45@hotmail.co.uk>
2022-05-25 18:50:51 +0100

    Add comment

--- cdk/lib/commercial-canaries.ts
+++ cdk/lib/commercial-canaries.ts
@@ -24,8 +24,10 @@ export class CommercialCanaries extends GuStack {
 
 		const email = 'commercial.canaries@guardian.co.uk';
 		const accountId = this.account;
-		const canaryName = `comm_cmp_canary_${stage.toLocaleLowerCase()}`;
 		const S3Bucket = `cw-syn-canary-${accountId}-ca-central-1`;
+
+		// Limitation of max 21 characaters and lower case. Pattern: ^[0-9a-z_\-]+$
+		const canaryName = `comm_cmp_canary_${stage.toLocaleLowerCase()}`;
 
 		const policyDocument = new iam.PolicyDocument({
 			statements: [
