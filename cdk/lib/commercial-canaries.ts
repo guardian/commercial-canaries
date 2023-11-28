81de55dd24bc9c1154fc078d529d79b80481ee9a
Dominik Lander <dominik.lander@guardian.co.uk>
2023-11-28 10:55:03 +0000

    bump puppeteer to v6.0

--- cdk/lib/commercial-canaries.ts
+++ cdk/lib/commercial-canaries.ts
@@ -126,7 +126,7 @@ export class CommercialCanaries extends GuStack {
 			},
 			executionRoleArn: role.roleArn,
 			name: canaryName,
-			runtimeVersion: 'syn-nodejs-puppeteer-4.0',
+			runtimeVersion: 'syn-nodejs-puppeteer-6.0',
 			runConfig: {
 				timeoutInSeconds: 60,
 				memoryInMb: isTcf ? 2048 : 3008,
