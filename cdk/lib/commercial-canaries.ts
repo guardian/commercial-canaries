39b7f5ca73470196a27e2d080c11e8db43e1840e
Charlotte <charlotte.emms@theguardian.com>
2025-03-20 12:45:14 +0000

    temporarily enable in CODE

--- cdk/lib/commercial-canaries.ts
+++ cdk/lib/commercial-canaries.ts
@@ -148,42 +148,42 @@ export class CommercialCanaries extends GuStack {
 		});
 
 		// We add the alarm only for PROD but it's easier to keep the topic and subscription in both stages.
-		if (stage === 'PROD') {
-			const successPercentFilled = new MathExpression({
-				// Make sure to fill in missing data with 0 before calculating the average
-				expression: 'FILL(METRICS(), 0)',
-				period: Duration.minutes(1),
-				usingMetrics: {
-					successPercent: new Metric({
-						namespace: 'CloudWatchSynthetics',
-						metricName: 'SuccessPercent',
-						statistic: 'avg',
-						period: Duration.minutes(1),
-						dimensionsMap: {
-							CanaryName: canaryName,
-						},
-					}),
-				},
-			});
-
-			const alarm = new Alarm(this, 'Alarm', {
-				actionsEnabled: true,
-				alarmDescription: `Either a Front or an Article CMP has failed in ${env.region}`,
-				alarmName: `commercial-canary-${stage}`,
-				comparisonOperator: ComparisonOperator.LESS_THAN_OR_EQUAL_TO_THRESHOLD,
-				datapointsToAlarm: 5,
-				evaluationPeriods: 5,
-				metric: new MathExpression({
-					expression: 'AVG(successPercentFilled)',
+		// if (stage === 'PROD') {
+		const successPercentFilled = new MathExpression({
+			// Make sure to fill in missing data with 0 before calculating the average
+			expression: 'FILL(METRICS(), 0)',
+			period: Duration.minutes(1),
+			usingMetrics: {
+				successPercent: new Metric({
+					namespace: 'CloudWatchSynthetics',
+					metricName: 'SuccessPercent',
+					statistic: 'avg',
 					period: Duration.minutes(1),
-					usingMetrics: { successPercentFilled },
+					dimensionsMap: {
+						CanaryName: canaryName,
+					},
 				}),
-				threshold: 80,
-				treatMissingData: TreatMissingData.BREACHING,
-			});
+			},
+		});
 
-			alarm.addAlarmAction(new SnsAction(topic));
-			alarm.addOkAction(new SnsAction(topic));
-		}
+		const alarm = new Alarm(this, 'Alarm', {
+			actionsEnabled: true,
+			alarmDescription: `Either a Front or an Article CMP has failed in ${env.region}`,
+			alarmName: `commercial-canary-${stage}`,
+			comparisonOperator: ComparisonOperator.LESS_THAN_OR_EQUAL_TO_THRESHOLD,
+			datapointsToAlarm: 5,
+			evaluationPeriods: 5,
+			metric: new MathExpression({
+				expression: 'AVG(successPercentFilled)',
+				period: Duration.minutes(1),
+				usingMetrics: { successPercentFilled },
+			}),
+			threshold: 80,
+			treatMissingData: TreatMissingData.BREACHING,
+		});
+
+		alarm.addAlarmAction(new SnsAction(topic));
+		alarm.addOkAction(new SnsAction(topic));
 	}
 }
+// }
