6408d2b0d6f3c505dac2938e42d65866d5b6780b
Charlotte <charlotte.emms@theguardian.com>
2025-04-07 10:26:49 +0100

    remove explicit role from CDK since this should be automatically generated

--- cdk/lib/commercial-canaries.ts
+++ cdk/lib/commercial-canaries.ts
@@ -1,12 +1,7 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
-import {
-	Duration,
-	aws_iam as iam,
-	Size,
-	aws_synthetics as synthetics,
-} from 'aws-cdk-lib';
+import { Duration, Size, aws_synthetics as synthetics } from 'aws-cdk-lib';
 import {
 	Alarm,
 	ComparisonOperator,
@@ -33,72 +28,8 @@ export class CommercialCanaries extends GuStack {
 		const s3BucketNameResults = `cw-syn-results-${accountId}-${env.region}`;
 		const isTcf = env.region === 'eu-west-1' || env.region === 'ca-central-1';
 
-		// Limitation of max 21 characaters and lower case. Pattern: ^[0-9a-z_\-]+$
-		const canaryName = `comm_cmp_canary_${stage.toLocaleLowerCase()}`;
-
-		const policyDocument = new iam.PolicyDocument({
-			statements: [
-				new iam.PolicyStatement({
-					resources: [`arn:aws:s3:::${s3BucketNameCanary}/*`],
-					actions: ['s3:PutObject', 's3:GetObject', 's3:GetBucketLocation'],
-					effect: iam.Effect.ALLOW,
-				}),
-				new iam.PolicyStatement({
-					resources: [`arn:aws:s3:::${s3BucketNameResults}/*`],
-					actions: [
-						's3:PutObject',
-						's3:GetObject',
-						's3:GetBucketLocation',
-						's3:DeleteObject',
-					],
-					effect: iam.Effect.ALLOW,
-				}),
-				new iam.PolicyStatement({
-					resources: [
-						`arn:aws:logs:${env.region}:${accountId}:log-group:/aws/lambda/cwsyn-${canaryName}-*`,
-					],
-					actions: [
-						'logs:CreateLogStream',
-						'logs:PutLogEvents',
-						'logs:CreateLogGroup',
-					],
-					effect: iam.Effect.ALLOW,
-				}),
-				new iam.PolicyStatement({
-					resources: ['*'],
-					actions: ['s3:ListAllMyBuckets', 'xray:PutTraceSegments'],
-					effect: iam.Effect.ALLOW,
-				}),
-				new iam.PolicyStatement({
-					resources: ['*'],
-					actions: ['cloudwatch:PutMetricData'],
-					effect: iam.Effect.ALLOW,
-					conditions: {
-						StringEquals: {
-							'cloudwatch:namespace': 'CloudWatchSynthetics',
-						},
-					},
-				}),
-			],
-		});
-
-		const role = new iam.Role(this, 'Role', {
-			assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-			roleName: `CloudWatchSyntheticsRole-${canaryName}-${env.region}`,
-			description:
-				'CloudWatch Synthetics lambda execution role for running canaries',
-			managedPolicies: [
-				{
-					managedPolicyArn:
-						'arn:aws:iam::aws:policy/CloudWatchSyntheticsFullAccess',
-				},
-			],
-			inlinePolicies: {
-				PolicyDocument: policyDocument,
-			},
-		});
-
 		const canary = new synthetics.Canary(this, 'Canary', {
+			canaryName: `comm_cmp_canary_${stage.toLocaleLowerCase()}`,
 			artifactsBucketLocation: {
 				bucket: Bucket.fromBucketName(
 					this,
@@ -113,14 +44,12 @@ export class CommercialCanaries extends GuStack {
 				),
 				handler: 'pageLoadBlueprint.handler',
 			}),
-			role,
-			canaryName,
 			runtime: synthetics.Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0,
 			schedule: synthetics.Schedule.rate(Duration.minutes(1)),
-			timeout: Duration.seconds(60),
-			memory: Size.mebibytes(isTcf ? 2048 : 3008),
 			// Don't run non-prod canaries indefinitely
 			timeToLive: stage === 'PROD' ? undefined : Duration.minutes(30),
+			timeout: Duration.seconds(60),
+			memory: Size.mebibytes(isTcf ? 2048 : 3008),
 			provisionedResourceCleanup: true,
 			successRetentionPeriod: Duration.days(7),
 			failureRetentionPeriod: Duration.days(31),
