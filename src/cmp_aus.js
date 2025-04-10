33c667d808ab4d6ea7d59f867cbe9231b0c7b99e
Charlotte <charlotte.emms@theguardian.com>
2025-04-10 16:54:38 +0100

    add extra two second wait in aus

--- src/cmp_aus.js
+++ src/cmp_aus.js
@@ -177,6 +177,7 @@ const checkPage = async (pageType, url) => {
 	// Now we can run our tests.
 	log(`[TEST 1] start: Adverts load and the CMP is displayed on initial load`);
 	await reloadPage(page);
+	await page.waitForTimeout(TWO_SECONDS); // Wait an extra two seconds after reloading the page
 	await synthetics.takeScreenshot(`${pageType}-page`, 'page loaded');
 	await checkCMPIsOnPage(page, pageType);
 	await checkTopAdHasLoaded(page, pageType);
