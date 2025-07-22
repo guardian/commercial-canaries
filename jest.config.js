5129939f04ac1552ceef26629cd74067422cfb73
Charlotte <charlotte.emms@theguardian.com>
2025-07-22 12:16:14 +0100

    bump eslint version, remove redundant eslint files, update commands for lint and format, run prettier on whole repo

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testMatch: ['<rootDir>/cdk/lib/**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  setupFilesAfterEnv: ["./jest.setup.js"],
+	testMatch: ['<rootDir>/cdk/lib/**/*.test.ts'],
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+	setupFilesAfterEnv: ['./jest.setup.js'],
 };
