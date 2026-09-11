ce6eafd1462cf9e68e1284f87cc069bfb7dd0c31
Charlotte <charlotte.emms@theguardian.com>
2026-09-11 10:41:41 +0100

    feed riffRaffProjectName into each canary

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -8,55 +8,56 @@ const cdkApp = new App();
 const stages = ['CODE', 'PROD'];
 const stack = 'frontend';
 const cloudFormationStackName = 'commercial-canary';
+const riffRaffProjectName = 'frontend::commercial-canaries';
 
 const canaryApps = stages
-	.map((stage) =>
-		regions.map(
-			({ locationAbbr, region, frontUrl, articleUrl, pageskinUrl }) => ({
-				app: new CommercialCanaries(
-					cdkApp,
-					`CommercialCanaries-${locationAbbr}-${stage}-front`,
-					{
-						stack,
-						stage,
-						env: { region },
-						cloudFormationStackName,
-						frontUrl,
-						articleUrl,
-						pageskinUrl,
-					},
-				),
-				locationAbbr,
-				region,
-			}),
-		),
-	)
-	.flat();
+  .map((stage) =>
+    regions.map(
+      ({ locationAbbr, region, frontUrl, articleUrl, pageskinUrl }) => ({
+        app: new CommercialCanaries(
+          cdkApp,
+          `CommercialCanaries-${locationAbbr}-${stage}-front`,
+          {
+            stack,
+            stage,
+            env: { region },
+            cloudFormationStackName,
+            frontUrl,
+            articleUrl,
+            pageskinUrl,
+            riffRaffProjectName
+          },
+        ),
+        locationAbbr,
+        region,
+      }),
+    ),
+  )
+  .flat();
 
 const riffRaff = new RiffRaffYamlFile(cdkApp);
 const { configuration } = riffRaff;
-const riffRaffProjectName = 'frontend::commercial-canaries';
 
 canaryApps.forEach(({ locationAbbr, region }) => {
-	configuration
-		.get(riffRaffProjectName)
-		?.deployments.set(`upload-${locationAbbr.toLowerCase()}`, {
-			type: 'aws-s3',
-			app: 'commercial-canaries',
-			regions: new Set([region]),
-			stacks: new Set([stack]),
-			parameters: {
-				bucketSsmKey: `/account/services/commercial-canary.bucket`,
-				cacheControl: 'private',
-				cloudFormationStackByTags: false,
-				cloudFormationStackName,
-				prefixPackage: false,
-				prefixStack: false,
-				prependStackToCloudFormationStackName: false,
-				publicReadAcl: false,
-			},
-			contentDirectory: `upload-${locationAbbr.toLowerCase()}`,
-		});
+  configuration
+    .get(riffRaffProjectName)
+    ?.deployments.set(`upload-${locationAbbr.toLowerCase()}`, {
+      type: 'aws-s3',
+      app: 'commercial-canaries',
+      regions: new Set([region]),
+      stacks: new Set([stack]),
+      parameters: {
+        bucketSsmKey: `/account/services/commercial-canary.bucket`,
+        cacheControl: 'private',
+        cloudFormationStackByTags: false,
+        cloudFormationStackName,
+        prefixPackage: false,
+        prefixStack: false,
+        prependStackToCloudFormationStackName: false,
+        publicReadAcl: false,
+      },
+      contentDirectory: `upload-${locationAbbr.toLowerCase()}`,
+    });
 });
 
 riffRaff.synth();
