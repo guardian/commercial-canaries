fb38e3be5d58d714074af475ae5e2d2b00c1ed7f
Dina Hafez <dina.hafez@guardian.co.uk>
2026-09-11 12:28:30 +0100

    Remove the legacy SyntheticsLogger, not compatible with playwright

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -1,14 +1,12 @@
-const logger = require('SyntheticsLogger');
-
 /**
  * We use custom log messages so that we can easily differentiate
  * between logs from this file and other logs in Cloudwatch.
  */
 const formatMessage = (message) => `GuCanaryRun: ${message}`;
 
-const log = (message) => logger.info(formatMessage(message));
+const log = (message) => console.log(formatMessage(message));
 
-const logError = (message) => logger.error(formatMessage(message));
+const logError = (message) => console.error(formatMessage(message));
 
 module.exports = {
 	log,
